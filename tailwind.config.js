ab11e0d225b0fa10b7452c10f7753191de3ea076
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2021-08-25 20:23:51 +0300

    Add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   purge: ['./src/**/*.pug'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['PTRootUI', 'Verdana', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  separator: '_',
 };
